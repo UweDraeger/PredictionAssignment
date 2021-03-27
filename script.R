52bceb80b59f8ee118a281a380ff00c6c19fc6d4
UweDraeger <uwe.draeger@gmx.net>
2021-03-27 19:39:01 +0100

    some first explorations

--- script.R
+++ script.R
@@ -1,2 +1,16 @@
-## script
+library(tidyverse)
+library(tidymodels)
+library(readr)
 
+# read the data file, set error to NA
+pml_training <-
+        as_tibble(read_csv("pml-training.csv", na = c("#DIV/0!", "NA"))) %>%
+        mutate_if(is.character, as.factor)
+
+
+pml_testing <-
+        as_tibble(read_csv("pml-testing.csv", na = c("#DIV/0!", "NA"))) %>%
+        mutate_if(is.character, as.factor)
+
+#find all variables with data in testing (i.e. remove all cols with NA only)
+predictors <- pml_testing %>% select(which(colMeans(is.na(.)) == 0)) 
