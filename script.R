0064c5501553a8216d57ff5ac4e980455fa1fafb
UweDraeger <uwe.draeger@gmx.net>
2021-03-29 17:58:25 +0200

    fit function returns error

--- script.R
+++ script.R
@@ -13,13 +13,14 @@ pml_training <-
         as_tibble(read_csv("pml-training.csv", na = c("#DIV/0!", "NA"))) %>%
         mutate_if(is.character, as.factor)
 
-pml_testing <-
+# to avoid confusion, the "testing" set is called newdata 
+pml_newdata <-
         as_tibble(read_csv("pml-testing.csv", na = c("#DIV/0!", "NA"))) %>%
         mutate_if(is.character, as.factor)
 
 # find all variables with usable data in testing (i.e. remove all cols with NA only)
-predictors <- pml_testing %>% select(which(colMeans(is.na(.)) == 0))
-prednames <- colnames(predictors)
+vars_with_data <- pml_newdata %>% select(which(colMeans(is.na(.)) == 0))
+prednames <- colnames(vars_with_data)
 
 # reduce training set to variables actually available in test set
 # predname[60] is problem_id (copy of X1)
@@ -30,46 +31,53 @@ train <- pml_training %>%
         select(-(X1:num_window)) %>%
         mutate(classe = pml_training$classe)
 
-# split train and validation, create cv object 
+# split train and test, create cross validation object 
 set.seed(12345)
 
 train_split <- initial_split(train, prop = 0.75)
-train_cv <- vfold_cv(train)
+pml_train <- training(train_split)
+pml_test <- testing(train_split)
+
+train_cv <- vfold_cv(pml_train)
 
 
-# preprocess (just normalize the numeric data) and define recipe
+# preprocess (just normalize the numeric data and deal with near zero variance 
+# data) and define recipe
 pml_recipe <-
-        recipe(classe ~ ., data = train) %>%
-        step_normalize(all_numeric())
+        recipe(classe ~ ., data = train_split) %>%
+        step_normalize(all_numeric()) %>%
+        step_zv(all_predictors())
 
-# spec the model to be random forest, tune mtry, ranger
+# spec the model to be random forest, tune mtry and trees, use ranger 
+# implementation of random forest 
 pml_model <- rand_forest() %>%
         set_args(mtry = tune(), trees = tune()) %>%
-        set_engine("ranger", importance = "impurity") %>%
+        set_engine("ranger") %>%
         set_mode("classification")
 
-# build the workflow
+# build the workflow 
 pml_workflow <- workflow() %>%
         add_recipe(pml_recipe) %>%
         add_model(pml_model)
 
 
-# tune mtry parameter
-pml_grid <- expand.grid(mtry = c(3,4,5), trees = c(2, 5, 10))
+# tune mtry and trees parameters
+pml_grid <- expand.grid(mtry = c(3, 5, 7), trees = c(20, 50, 100))
 pml_tune_results <- pml_workflow %>%
         tune_grid(
                 resamples = train_cv,
                 grid = pml_grid,
                 metrics = metric_set(accuracy, sens, spec)
         )
-
-# set mtry parameter in final workflow
-# choice of accuracy over roc_auc since roc_auc = 1 for all params
-mtry_final <- pml_tune_results %>%
+results <- pml_tune_results %>% collect_metrics()
+ 
+# set parameters in final workflow
+# choice of accuracy 
+tuning_result <- pml_tune_results %>%
         select_best(metric = "accuracy")
 
 pml_final_workflow <- pml_workflow %>%
-        finalize_workflow(mtry_final)
+        finalize_workflow(tuning_result)
         
 # evaluate model on validation set
 pml_fit <- pml_final_workflow %>%
@@ -84,10 +92,13 @@ validat_pred <- pml_fit %>%
 validat_pred %>%
         conf_mat(truth = classe, estimate = .pred_class)
 
- res <- pml_tune_results %>% collect_metrics()
+
 # fitting final model
+class(pml_fit)
+pml_final <- fit(pml_fit, pml_training)
 
 # predict test data
 
+predict(pml_final, new_data = pml_newdata)
 stopCluster(cluster)
 registerDoSEQ()
